4a0a1ebe8362941167c7794833a1e0310f951262
alirezamou <am808036@gmail.com>
2023-02-18 16:27:31 +0330

    markdown editor in textarea

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script>
+import { marked } from 'marked';
+import { debounce } from "lodash-es";
+export default {
+  data() {
+    return {
+      input: "# hello"
+    }
+  },
+  computed: {
+    output() {
+      return marked(this.input)
+    }
+  },
+  methods: {
+    update: debounce(function (e) {
+      this.input = e.target.value;
+    }, 100)
+  }
+}
 </script>
 
 <template>
+  <textarea :value="input" @input="update"></textarea>
+  <div v-html="output"></div>
 </template>
 
 <style>
